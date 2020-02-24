0c51e508e656d4d353d616b00c7c555aa84cebc7
vwhwang <vwhwang38@gmail.com>
2020-02-24 11:19:14 -0800

    deck method and test added

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,15 +5,28 @@ require_relative 'card'
 
 class Deck
 
-  def initialize
+  attr_reader :decks
 
+  def initialize
+    valid_value = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    valid_suit = [:hearts, :spades, :clubs, :diamonds]
+    @decks = []
+    valid_value.each do |value|
+      valid_suit.each do |suit|
+        card = Card.new(value,suit)
+        @decks << card
+      end 
+    end 
   end
 
   def draw
     # returns a card
+    card = decks.sample
+    @decks = @decks.reject {|cards| cards == card}
+    return card 
   end
 
   def shuffle
-    # shuffles the deck
+    @decks.shuffle!
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,4 +6,48 @@ describe Deck do
     # 1.  Expected Behavior
     # 2.  Edge cases
 
+  describe "Deck can create instances" do 
+
+    it "deck should have 52 cards" do 
+      deck_length = Deck.new.decks.length 
+      expect(deck_length).must_equal 52
+    end 
+
+    it "each card in decks should be a Card class" do 
+      deck = Deck.new
+      expect(deck.decks[0]).must_be_kind_of Card
+    end 
+
+  end 
+
+  describe "draw method" do 
+    it "return a card class when draw" do 
+      card = Deck.new.draw
+      expect(card).must_be_kind_of Card
+    end 
+
+    it "read the card from that was drew" do 
+      card = Deck.new.draw
+      expect(card.to_s).must_be_kind_of String
+      expect([:hearts, :spades, :clubs, :diamonds].include?(card.suit)).must_equal true
+    end 
+    it "will return 2 less card" do 
+      decks = Deck.new
+      decks.draw
+      decks.draw
+      expect(decks.decks.length).must_equal 50
+    end 
+
+    it "will return false since draw card doesn't exist in deck" do 
+      decks = Deck.new
+      card = decks.draw
+      expect(decks.decks.include?(card)).must_equal false 
+    end 
+
+  end 
+
+  # describe "Shuffle method" do 
+  #   it "return "
+  # end 
+
 end
